42ca8022b29f29b7a7bea87fc382df2305dc9bd1
kavinila05 <129657981+kavinila05@users.noreply.github.com>
2026-03-31 22:01:06 +0530

    Create class_labels.py

---
+++ src/vision/class_labels.py
@@ -0,0 +1,23 @@
+TOMATO_CLASSES = [
+    'Tomato__Bacterial_spot',
+    'Tomato_Early_blight',
+    'Tomato_Late_blight',
+    'Tomato_Leaf_Mold',
+    'Tomato_Septoria_leaf_spot',
+    'Tomato_Spider_mites',
+    'Tomato_Yellow_Leaf_Curl_Virus',
+    'Tomato_Mosaic_virus',
+    'Tomato_healthy'
+]
+
+PEST_CLASSES = [
+    'mites',
+    'armyworm',
+    'beetle',
+    'bollworm',
+    'grasshopper',
+    'aphids',
+    'mosquito',
+    'sawfly',
+    'stem_borer'
+]
